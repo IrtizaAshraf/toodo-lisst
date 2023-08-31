565ff5f384d71b7b22369af738bc33df3a25d8a1
Irtiza Ashraf <irtizaashraf802@gmail.com>
2023-08-31 17:11:44 +0500

    create

--- app.js
+++ app.js
@@ -8,25 +8,26 @@ function submit() {
 }
 
 
+//paractice 1
+
+// function create2() {
+//     const listValue = + document.querySelector('#list').value;
+//     const output = document.querySelector('#output');
+
+//     // Clear previous content
+//     output.innerHTML = listValue;
 
 
-function create2() {
-    const listValue = + document.querySelector('#list').value;
-    const output = document.querySelector('#output');
+//     const itemCount = 5;
 
-    // Clear previous content
-    output.innerHTML = listValue;
+//     for (let i = 1; i <= 10; i++) {
+//         const listItem = document.createElement('p');
+//         listItem.textContent = `${listValue} - Item ${i}`;
+//         output.appendChild(listItem);
+//     }
+// }
 
-
-    const itemCount = 5;
-
-    for (let i = 1; i <= 10; i++) {
-        const listItem = document.createElement('p');
-        listItem.textContent = `${listValue} - Item ${i}`;
-        output.appendChild(listItem);
-    }
-}
-
+//practice 2
 
 
 // const additem = document.querySelector("#send");
@@ -48,6 +49,9 @@ function create2() {
 // }
 
 
+
+//solution .....
+
 const additem = document.querySelector("#send");
 const arry = document.querySelector("#list");
 
@@ -62,9 +66,44 @@ function create() {
 
     for (let g = 0; g < arr.length; g++) {
         const item = arr[g];
-        arry.innerHTML += `<li>${item}</li> < 
-        <button>Delete</button>
-        <button>Edit</button>`
+        arry.innerHTML += `<li>${item}</li> 
+        <button onclick="deletelist(${g})">Delete</button>
+        <button onclick="editlist(${g})">Edit</button>`
         ;
     }
 }
+
+
+
+
+function deletelist(index) {
+    arr.splice(index, 1) 
+    render();  
+}
+
+
+function editlist(index) {
+   const content = prompt('Edit task:', arr[index]);
+    if (content !== null) {
+        arr[index] = content ;
+        render();
+    }
+}
+
+
+function render() {
+
+    arry.innerHTML = ""; // Clear the previous list content
+    for (let g = 0; g < arr.length; g++) {
+        const item = arr[g];
+        arry.innerHTML += `<li>${item}</li>  
+        <button onclick="deletelist(${g})">Delete</button>
+        <button onclick="editlist(${g})">Edite</button>`
+        ;
+    }
+    
+}
+
+
+
+
