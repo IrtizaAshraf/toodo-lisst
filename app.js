01164fca42d81be37834ddec63c52f09bc3650cf
Irtiza Ashraf <irtizaashraf802@gmail.com>
2023-09-02 18:56:54 +0500

    done

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
-const item = document.querySelector("#value").value;
-function submit() {
+// const item = document.querySelector("#value").value;
+// function submit() {
 
-    console.log(item)
-    // console.log(item);
+//     console.log(item)
+//     // console.log(item);
 
-    item = ''
-}
-
-
+//     item = ''
+// }
 
 
-function create2() {
-    const listValue = + document.querySelector('#list').value;
-    const output = document.querySelector('#output');
-
-    // Clear previous content
-    output.innerHTML = listValue;
 
 
-    const itemCount = 5;
+// function create2() {
+//     const listValue = + document.querySelector('#list').value;
+//     const output = document.querySelector('#output');
 
-    for (let i = 1; i <= 10; i++) {
-        const listItem = document.createElement('p');
-        listItem.textContent = `${listValue} - Item ${i}`;
-        output.appendChild(listItem);
-    }
-}
+//     // Clear previous content
+//     output.innerHTML = listValue;
+
+
+//     const itemCount = 5;
+
+//     for (let i = 1; i <= 10; i++) {
+//         const listItem = document.createElement('p');
+//         listItem.textContent = `${listValue} - Item ${i}`;
+//         output.appendChild(listItem);
+//     }
+// }
 
 
 
@@ -48,105 +48,132 @@ function create2() {
 // }
 
 
-const additem = document.querySelector("#add");
-const arry = document.querySelector("#list");
-const errormassage = document.querySelector("#error-message")
-const arr = [];
 
-function create() {
-    if (additem.value =="") {
-        alert("plese enter the value")
-        
-    } 
-    
-    else{
-        arr.push(additem.value);
-        console.log(arr);
-        additem.value = "";
-    
-        arry.innerHTML = ""; // Clear the previous list content
-    
-        for (let g = 0; g < arr.length; g++) {
-            const item = arr[g];
-            arry.innerHTML += `<li>${item}</li>  
-            <button onclick="deletelist(${g})">Delete</button>
-            <button onclick="editlist(${g})">Edite</button>`
-            ;
-        }
-    }
-
-        
-    
-}
+//solutionn todo list
 
 
-function deletelist(index) {
-    arr.splice(index, 1) 
-    render();  
-}
+// const additem = document.querySelector("#add");
+// const arry = document.querySelector("#list");
+// const errormassage = document.querySelector("#error-message")
+// const arr = [];
 
+// function create() {
+//     if (additem.value =="") {
+//         alert("plese enter the value")
 
-function editlist(index) {
-   const content = prompt('Edit task:', arr[index]);
-    if (content === "") {
-        arr[index] = content ;
-        // render();
-        alert("please Enter the value")
-    }
-    else{
-        arr[index] = content ;
-        render();
-    }
-}
+//     } 
 
+//     else{
+//         arr.push(additem.value);
+//         console.log(arr);
+//         additem.value = "";
 
-function render() {
+//         arry.innerHTML = ""; // Clear the previous list content
 
-    arry.innerHTML = ""; // Clear the previous list content
-    for (let g = 0; g < arr.length; g++) {
-        const item = arr[g];
-        arry.innerHTML += `<li>${item}</li>  
-        <button onclick="deletelist(${g})">Delete</button>
-        <button onclick="editlist(${g})">Edite</button>`
-        ;
-    }
-
-}
+//         for (let g = 0; g < arr.length; g++) {
+//             const item = arr[g];
+//             arry.innerHTML += `<li>${item}</li>  
+//             <button onclick="deletelist(${g})">Delete</button>
+//             <button onclick="editlist(${g})">Edite</button>`
+//             ;
+//         }
+//     }
 
 
 
+// }
 
 
+// function deletelist(index) {
+//     arr.splice(index, 1) 
+//     render();  
+// }
 
 
-
-
-// const addlist = document.querySelector("#add");
-// const arrry = document.querySelector("#list");
-// const erroermassage = document.querySelector("#error-message")
-// const todo = [];
-
-// function todolist() {
-//     switch(addlist){
-//         addlist.value =='';
-//         alert("please enter the value")
-//             break;
-    
-//         default:
-            
-//             todo.push(addlist.value);
-//             console.log(todo);
-//             addlist.value = "";
-//             arrry.innerHTML = ""; // Clear the previous list content
-//             for (let h = 0; h < todo.length; h++) {
-//             const item = todo[h];
-//             arrry.innerHTML += `<li>${item}</li>  
-//             <button onclick="deletelist(${h})">Delete</button>
-//             <button onclick="editlist(${h})">Edite</button>`;
-//          }
-
-//         break;
-              
+// function editlist(index) {
+//    const content = prompt('Edit task:', arr[index]);
+//     if (content === "") {
+//         arr[index] = content ;
+//         // render();
+//         alert("please Enter the value")
+//     }
+//     else{
+//         arr[index] = content ;
+//         render();
 //     }
 // }
 
+
+// function render() {
+
+//     arry.innerHTML = ""; // Clear the previous list content
+//     for (let g = 0; g < arr.length; g++) {
+//         const item = arr[g];
+//         arry.innerHTML += `<li>${item}</li>  
+//         <button onclick="deletelist(${g})">Delete</button>
+//         <button onclick="editlist(${g})">Edite</button>`
+//         ;
+//     }
+
+// }
+
+
+
+
+
+
+
+
+
+const addlist = document.querySelector("#add");
+const arrry = document.querySelector("#list");
+const erroermassage = document.querySelector("#error-message")
+const todo = [];
+
+function todolist(e) {
+    e.preventDefault();
+
+    switch (addlist.value) {
+        case '':
+            alert("Enter the Value ")
+
+            break;
+
+        default:
+            todo.push(addlist.value)
+            addlist.value = '';
+            arrry.innerHTML = '';
+            for (let g = 0; g < todo.length; g++) {
+                const item = todo[g];
+                arrry.innerHTML += `<li>${item}</li>  
+             <button onclick="deletvalue(${g})">Delete</button>
+             <button onclick="editvalue(${g})">Edite</button>`
+                    ;
+            }
+            break;
+    }
+
+}
+
+function deletvalue(index) {
+    todo.splice(index, 1)
+    addlist.value = '';
+    arrry.innerHTML = '';
+    for (let g = 0; g < todo.length; g++) {
+        const item = todo[g];
+        arrry.innerHTML += `<li>${item}</li>  
+             <button onclick="deletvalue(${g})">Delete</button>
+             <button onclick="editvalue(${g})">Edite</button>`
+            ;
+    }
+
+}
+
+
+function editvalue(index) {
+    const todolist = +prompt("Change the Value", todo[index]) 
+    todo.splice(index, 1, todolist)
+
+
+    
+}
